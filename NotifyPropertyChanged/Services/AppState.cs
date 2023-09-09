6f91a0d93c397028b7b1f6e33731852dbf4ed4e5
polaarbear <aar.j.smith@gmail.com>
2023-09-09 18:02:37 -0500

    Add project files.

---
+++ NotifyPropertyChanged/Services/AppState.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace NotifyPropertyChanged.Services
+{
+    public class AppState : INotifyPropertyChanged
+    {
+        private string _ChangeableString = "Initial Changeable String";
+        public string ChangeableString
+        {
+            get { return _ChangeableString; }
+            set
+            {
+                if(_ChangeableString != value)
+                {
+                    _ChangeableString = value;
+                    OnPropertyChanged(nameof(ChangeableString));
+                }
+            }        
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
